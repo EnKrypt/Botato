723ac172d5e8fb7464cc8d799c4b159ec434acd0
Arvind Kumar <arvind@enkrypt.in>
2016-09-02 23:45:01 +0530

    Fixed authorization bugs

--- config.js
+++ config.js
@@ -6,12 +6,16 @@ var fs = require('fs'),
 var config = {};
 var warning = "";
 
+var showWarning = function(warn) {
+    rls.question(warn, {hideEchoBack: true, mask: ''});
+};
+
 try {
     var rcFile = fs.readFileSync('.botatorc', 'utf8');
     try {
         config = JSON.parse(rcFile);
     } catch (ex) {
-        warning = 'The .botatorc file that was found is not valid JSON. Press Enter to use default config values';
+        showWarning('The .botatorc file that was found is not valid JSON. Press Enter to use default config values'); //Show the warning right here as there is no other appropriate place to do so
     }
 } catch (e) {
     warning = 'No .botatorc file found in working directory. It is recommended to create one to password protect your bot and skip manually typing runtime arguments. With the default config values, everyone in the same network will have shell access to your device. Press Enter if you know what you\'re doing';
@@ -32,9 +36,7 @@ module.exports = {
     usePassword: config.usePassword || false,
     password: config.password,
     warn: warning,
-    showWarning: function(warn) {
-        rls.question(warn, {hideEchoBack: true, mask: ''});
-    },
+    showWarning: showWarning,
     //Offers basic protection from attacks via network-type and command names
     noSpecialChars: function(str) {
         if (/^[a-zA-Z0-9- ]*$/.test(str)) {
--- parse.js
+++ parse.js
@@ -3,7 +3,7 @@
 module.exports = function(bot, from, args) {
     var out = bot.connection.send.bind(bot.connection);
     var command = args[0].slice(bot.commandPrefix.length).toLowerCase();
-    if (bot.usePassword || (bot.authorized.indexOf(from) > -1 || command == 'auth')) {
+    if ((!bot.usePassword) || (bot.authorized.indexOf(from) > -1 || command == 'auth')) {
         if ((args[0].length > bot.commandPrefix.length) && args[0].startsWith(bot.commandPrefix)) {
             try {
                 require('./commands/' + bot.noSpecialChars(command))(bot, from, args.slice(1), out);

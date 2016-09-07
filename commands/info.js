9c08bd401c1b89a5210e5306904ba20ca6dad39d
Arvind Kumar <arvind@enkrypt.in>
2016-09-07 21:09:39 +0530

    Modified !info to comply with its spec

--- commands/info.js
+++ commands/info.js
@@ -1,12 +1,25 @@
 'use strict';
 
 module.exports = function(bot, from, args, out) {
-    out(bot.name + ' - version ' + bot.version);
-    out('Release: ' + bot.release);
-    out('Using password: ' + bot.usePassword);
-    out(bot.shells.length + ' shell(s) open');
-    out(bot.history.length + ' command(s) run');
-    if (bot.hasUpdate) {
-        out('There are pending updates. Restart this bot to apply them');
+    //If no args, display verbose
+    if (!args.length) {
+        out(bot.name + ' - version ' + bot.version);
+        out('Release: ' + bot.release);
+        out('Using password: ' + bot.usePassword);
+        out(bot.shells.length + ' shell(s) open');
+        out(bot.history.length + ' command(s) run');
+        if (bot.hasUpdate) {
+            out('There are pending updates. Restart this bot to apply them');
+        }
+    } else if (args[0].toLowerCase() == 'version') {
+        out(bot.version);
+    } else if (args[0].toLowerCase() == 'updates') {
+        if (bot.hasUpdate) {
+            out('There are pending updates. Restart this bot to apply them');
+        } else {
+            out('No updates waiting to be applied. Run !update to check for available updates');
+        }
+    } else {
+        out('Invalid parameter', true);
     }
 }
